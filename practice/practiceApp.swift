64682c37ad3a5b33a81645fd5a2a7393eca57693
rhapsody <kriti.vanipenta@gmail.com>
2024-07-12 10:02:13 -0700

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Kriti Vanipenta on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
